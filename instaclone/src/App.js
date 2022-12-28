2b4d218ffa4971ca7838b71728f2393920182449
puggioni <agustinpuggioni98@gmail.com>
2022-12-28 14:56:42 -0300

    apollo config

--- instaclone/src/App.js
+++ instaclone/src/App.js
@@ -1,9 +1,14 @@
 import { Button } from "semantic-ui-react";
 import React from "react";
+import client from "./config/apollo";
+import { ApolloProvider } from "@apollo/client";
+
 export default function App() {
   return (
-    <div className="app">
-      <Button>Click Here</Button>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="app">
+        <Button>Click Here</Button>
+      </div>
+    </ApolloProvider>
   );
 }
